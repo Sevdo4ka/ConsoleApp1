2c6df3db453b40d0a034c04196ee1c7d90753c9c
qdedm <qdedmangg@gmail.com>
2024-12-26 19:58:21 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            //Для 2 челых чисел
+            //Console.Write("a: ");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("b: ");
+            //int b = int.Parse(Console.ReadLine());
+            //int c = a + b;
+            //Console.WriteLine(c);
+
+            //Для 2 вещественных чисел
+            //Console.Write("a: ");
+            //double a = double.Parse(Console.ReadLine());
+            //Console.Write("b: ");
+            //double b = double.Parse(Console.ReadLine());
+            //double c = a + b;
+            //Console.WriteLine(c);
+
+            //Для 3 челых чисел
+            //Console.Write("a: ");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("b: ");
+            //int b = int.Parse(Console.ReadLine());
+            //Console.Write("c: ");
+            //int c = int.Parse(Console.ReadLine());
+            //int d = a + b + c;
+            //Console.WriteLine(d);
+
+            //Для 3 вещественных чисел
+            //Console.Write("a: ");
+            //double a = double.Parse(Console.ReadLine());
+            //Console.Write("b: ");
+            //double b = double.Parse(Console.ReadLine());
+            //Console.Write("c: ");
+            //double c = double.Parse(Console.ReadLine());
+            //double d = a + b + c;
+            //Console.WriteLine(d);
+
+            //2
+            //Для целых 
+            //Console.Write("side1 = ");
+            //int side1 = int.Parse(Console.ReadLine());
+            //Console.Write("side2 = ");
+            //int side2 = int.Parse(Console.ReadLine());
+            //int P = 2 * (side1 + side2);
+            //int S = side1 * side2;
+            //Console.WriteLine($"Периметр = {P}, Площадь = {S}");
+
+            //Для вещественных
+            //Console.Write("side1 = ");
+            //double side1 = double.Parse(Console.ReadLine());
+            //Console.Write("side2 = ");
+            //double side2 = double.Parse(Console.ReadLine());
+            //double P = 2 * (side1 + side2);
+            //double S = side1 * side2;
+            //Console.WriteLine($"Периметр = {P}, Площадь = {S}");
+
+            //3
+            //Console.Write("a= ");
+            //double a = double.Parse(Console.ReadLine());
+            //Console.Write("b= ");
+            //double b = double.Parse(Console.ReadLine());
+            //double c = Math.Sqrt(a * a + b * b);
+            //Console.WriteLine(c);  
+
+            //4
+            //Для целых
+            //Console.Write("side= ");
+            //int side = int.Parse(Console.ReadLine());
+            //int P = side * 4;
+            //int S = side * side;
+            //Console.WriteLine($"Периметр = {P}, Площадь = {S}");
+
+            //для вещественных
+            //Console.Write("side= ");
+            //double side = double.Parse(Console.ReadLine());
+            //double P = side * 4;
+            //double S = side * side;
+            //Console.WriteLine($"Периметр = {P}, Площадь = {S}");
+
+            Console.ReadKey();
+        }
+    }
+}
